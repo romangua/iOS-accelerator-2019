f4119eec77995b86e80bee5583b72654dddbff5a
Julio Castillo <juliocastillo.sistemas@gmail.com>
2019-01-09 22:04:43 -0300

    creacion del proyecto

---
+++ MyAppOne/MyAppOne/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyAppOne
+//
+//  Created by Julio Castillo on 9/1/19.
+//  Copyright © 2019 iOS-accelerator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyAppOne/MyAppOne/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyAppOne
+//
+//  Created by Julio Castillo on 9/1/19.
+//  Copyright © 2019 iOS-accelerator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
