62826d8003571a71c20c7b92635c50487fa55d0c
Román Guarino <romanguarino@hotmail.com>
2019-01-25 11:14:05 -0300

    First commit swift

---
+++ MyAppOneSwift/MyAppOneSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyAppOneSwift
+//
+//  Created by Roman Guarino on 25/01/2019.
+//  Copyright © 2019 Román Guarino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
